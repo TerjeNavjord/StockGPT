14ccdedf54a40311e9cb73bac679843df388cdbb
Terje Navjord <terje.navjord@gmail.com>
2024-08-19 13:59:27 +0200

    testing deployment to Azure

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # StockGPT
+
+
+
+### Environment Variables
+REACT_APP_API_KEY needs to be set to an API Key from financialmarketsprep.com
+REACT_APP_BACKEND_BASE_URL needs to be set to point to where the backend is available
--- frontend/src/Helpers/AxiosInstance.tsx
+++ frontend/src/Helpers/AxiosInstance.tsx
@@ -4,7 +4,5 @@ const apiClient = axios.create({
   baseURL: process.env.REACT_APP_BACKEND_BASE_URL,
 });
 
- // console.log(process.env.REACT_APP_BACKEND_BASE_URL);
-// console.log(apiClient.defaults.baseURL);
 
 export default apiClient;
