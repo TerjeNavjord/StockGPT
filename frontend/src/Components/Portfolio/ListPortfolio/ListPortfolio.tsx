68e625662497e0409ec7c4ab69cb7fe892851e44
Terje Navjord <terje.navjord@gmail.com>
2024-08-20 10:54:40 +0200

    fix ListPortfolio.tsx

--- frontend/src/Components/Portfolio/ListPortfolio/ListPortfolio.tsx
+++ frontend/src/Components/Portfolio/ListPortfolio/ListPortfolio.tsx
@@ -1,4 +1,4 @@
-import React, { SyntheticEvent } from "react";
+import React, { SyntheticEvent, useEffect } from "react";
 import CardPortfolio from "../CardPortfolio/CardPortfolio";
 import { PortfolioGet } from "../../../Models/Portfolio";
 
@@ -8,6 +8,13 @@ interface Props {
 }
 
 const ListPortfolio = ({ portfolioValues, onPortfolioDelete }: Props) => {
+
+  useEffect(() => {
+    console.log("portfolioValues:", portfolioValues);
+    console.log("Type of portfolioValues:", typeof portfolioValues);
+    console.log("Is Array:", Array.isArray(portfolioValues));
+  }, [portfolioValues]);
+
   return (
     <section id="portfolio">
       <h2 className="mb-3 mt-3 text-3xl font-semibold text-center md:text-4xl">
@@ -15,20 +22,19 @@ const ListPortfolio = ({ portfolioValues, onPortfolioDelete }: Props) => {
       </h2>
       <div className="relative flex flex-col items-center max-w-5xl mx-auto space-y-10 px-10 mb-5 md:px-6 md:space-y-0 md:space-x-7 md:flex-row">
         <>
-          {portfolioValues.length > 0 ? (
-            portfolioValues.map((portfolioValue) => {
-              return (
-                <CardPortfolio
-                  portfolioValue={portfolioValue}
-                  onPortfolioDelete={onPortfolioDelete}
-                />
-              );
-            })
-          ) : (
-            <h3 className="mb-3 mt-3 text-xl font-semibold text-center md:text-xl">
-              Your portfolio is empty.
-            </h3>
-          )}
+        {Array.isArray(portfolioValues) && portfolioValues.length > 0 ? (
+          portfolioValues.map((portfolioValue) => (
+           <CardPortfolio
+             key={portfolioValue.id}
+             portfolioValue={portfolioValue}
+             onPortfolioDelete={onPortfolioDelete}
+            />
+        ))
+) : (
+  <h3 className="mb-3 mt-3 text-xl font-semibold text-center md:text-xl">
+    Your portfolio is empty.
+  </h3>
+)}
         </>
       </div>
     </section>
