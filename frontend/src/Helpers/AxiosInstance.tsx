074d8898f4c7b624b543bc3905904f0c8ed5b541
Terje Navjord <terje.navjord@gmail.com>
2024-08-20 13:06:29 +0200

    test backup uri

--- frontend/src/Helpers/AxiosInstance.tsx
+++ frontend/src/Helpers/AxiosInstance.tsx
@@ -2,9 +2,10 @@ import axios from "axios";
 
 const baseURL = process.env.REACT_APP_BACKEND_BASE_URL;
 console.log("Base URL:", baseURL);
+const backupURL = process.env.REACT_APP_BACKEND_BASE_URL || "https://stockgpt-bdepetb7dza2fzfz.norwayeast-01.azurewebsites.net/api";
 
 const apiClient = axios.create({
-  baseURL: baseURL,  
+  baseURL: backupURL,  
   headers: {
     "Content-Type": "application/json",
   },
