d61f4240f538b7e224d7d5fb09f44eee2cd8de06
Terje Navjord <terje.navjord@gmail.com>
2024-08-20 12:41:42 +0200

    fix api routes

--- frontend/src/Helpers/AxiosInstance.tsx
+++ frontend/src/Helpers/AxiosInstance.tsx
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: process.env.REACT_APP_BACKEND_BASE_URL,
+  baseURL: process.env.REACT_APP_BACKEND_BASE_URL,  
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 
