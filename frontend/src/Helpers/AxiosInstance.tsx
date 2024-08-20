41f279ca5fb970cdad46af782adf590195b9951f
Terje Navjord <terje.navjord@gmail.com>
2024-08-20 12:52:01 +0200

    fixing errors

--- frontend/src/Helpers/AxiosInstance.tsx
+++ frontend/src/Helpers/AxiosInstance.tsx
@@ -1,7 +1,10 @@
 import axios from "axios";
 
+const baseURL = process.env.REACT_APP_BACKEND_BASE_URL;
+console.log("Base URL:", baseURL);
+
 const apiClient = axios.create({
-  baseURL: process.env.REACT_APP_BACKEND_BASE_URL,  
+  baseURL: baseURL,  
   headers: {
     "Content-Type": "application/json",
   },
